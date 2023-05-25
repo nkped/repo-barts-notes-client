a1fe9d8ce99cb46b4487460766cc309bcb84b66e
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-05-25 15:02:44 +0200

    more search

---
+++ src/SearchItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SearchItem = ({ search, setSearch }) => {
+  return (
+    <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        
+        <input 
+            type='text' 
+            placeholder='type to search..' 
+            role='searchbox' 
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)}
+             />
+    </form>
+  )
+}
+
+export default SearchItem
